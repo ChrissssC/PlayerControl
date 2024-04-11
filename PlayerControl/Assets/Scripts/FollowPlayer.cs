f906dd2281e2c28425dcb5a703c37392f2588978
Chris <chevezc@roverkids.org>
2024-04-11 13:53:46 -0400

    Finished Lesson 1.3.1

--- PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
     }
 }
